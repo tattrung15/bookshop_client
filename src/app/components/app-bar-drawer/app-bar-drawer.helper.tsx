3be45ca34b1ca15860c044d4f2564f68f28dbea9
Tất Trung <55653291+tattrung15@users.noreply.github.com>
2022-03-18 13:19:14 +0700

    fix app bar drawer helper

--- src/app/components/app-bar-drawer/app-bar-drawer.helper.tsx
+++ src/app/components/app-bar-drawer/app-bar-drawer.helper.tsx
@@ -3,127 +3,33 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
-  Icon,
-  ListSubheader,
   Tooltip,
 } from "@material-ui/core";
-import {
-  Dashboard as DashboardIcon,
-  People as PeopleIcon,
-  Assignment as AssignmentIcon,
-  Receipt as ReceiptIcon,
-} from "@material-ui/icons";
+import { MenuItem } from "@app/shared/types/menu-item.type";
 
-export const mainListItems = (
-  <div>
-    <Link to="/" style={{ color: "black", textDecoration: "none" }}>
-      <Tooltip title="Home" placement="right" arrow>
-        <ListItem button>
-          <ListItemIcon>
-            <Icon className="fa fa-home" />
-          </ListItemIcon>
-          <ListItemText primary="Home" />
-        </ListItem>
-      </Tooltip>
-    </Link>
-    <Link to="/admin" style={{ color: "black", textDecoration: "none" }}>
-      <Tooltip title="Dashboard" placement="right" arrow>
-        <ListItem button>
-          <ListItemIcon>
-            <DashboardIcon />
-          </ListItemIcon>
-          <ListItemText primary="Dashboard" />
-        </ListItem>
-      </Tooltip>
-    </Link>
-    <Link
-      to="/admin/user-management"
-      style={{ color: "black", textDecoration: "none" }}
-    >
-      <Tooltip title="User management" placement="right" arrow>
-        <ListItem button>
-          <ListItemIcon>
-            <PeopleIcon />
-          </ListItemIcon>
-          <ListItemText primary="Users" />
-        </ListItem>
-      </Tooltip>
-    </Link>
-    <Link
-      to="/admin/category-management"
-      style={{ color: "black", textDecoration: "none" }}
-    >
-      <Tooltip title="Category management" placement="right" arrow>
-        <ListItem button>
-          <ListItemIcon>
-            <Icon className="fa fa-list-alt" />
-          </ListItemIcon>
-          <ListItemText primary="Categories" />
-        </ListItem>
-      </Tooltip>
-    </Link>
-    <Link
-      to="/admin/product-management"
-      style={{ color: "black", textDecoration: "none" }}
-    >
-      <Tooltip title="Book management" placement="right" arrow>
-        <ListItem button>
-          <ListItemIcon>
-            <Icon className="fa fa-book" />
-          </ListItemIcon>
-          <ListItemText primary="Products" />
-        </ListItem>
-      </Tooltip>
-    </Link>
-    <Link
-      to="/admin/product-image-management"
-      style={{ color: "black", textDecoration: "none" }}
-    >
-      <Tooltip title="Images management" placement="right" arrow>
-        <ListItem button>
-          <ListItemIcon>
-            <Icon className="fa fa-image" />
-          </ListItemIcon>
-          <ListItemText primary="Products Images" />
-        </ListItem>
-      </Tooltip>
-    </Link>
-    <Link
-      to="/admin/sale-order-management"
-      style={{ color: "black", textDecoration: "none" }}
-    >
-      <Tooltip title="Sale orders management" placement="right" arrow>
-        <ListItem button>
-          <ListItemIcon>
-            <ReceiptIcon />
-          </ListItemIcon>
-          <ListItemText primary="Sale orders" />
-        </ListItem>
-      </Tooltip>
-    </Link>
-  </div>
-);
-
-export const secondaryListItems = (
-  <div>
-    <ListSubheader inset>Saved reports</ListSubheader>
-    <ListItem button>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Current month" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Last quarter" />
-    </ListItem>
-    <ListItem button>
-      <ListItemIcon>
-        <AssignmentIcon />
-      </ListItemIcon>
-      <ListItemText primary="Year-end sale" />
-    </ListItem>
-  </div>
-);
+export const renderListItems = (listItems: MenuItem[]) => {
+  return (
+    <div>
+      {!!listItems.length &&
+        listItems.map((item: MenuItem, index: number) => {
+          const Icon = item.icon;
+          return (
+            <Link
+              key={index}
+              to={item.linkTo}
+              style={{ color: "black", textDecoration: "none" }}
+            >
+              <Tooltip title={item.tooltip} placement="right" arrow>
+                <ListItem button>
+                  <ListItemIcon>
+                    <Icon />
+                  </ListItemIcon>
+                  <ListItemText primary={item.mainContent} />
+                </ListItem>
+              </Tooltip>
+            </Link>
+          );
+        })}
+    </div>
+  );
+};
