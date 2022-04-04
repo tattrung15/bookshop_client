5e030025a698328d41f7d18050f906911edc9122
Tất Trung <55653291+tattrung15@users.noreply.github.com>
2022-04-04 17:23:17 +0700

    rename role service

--- src/app/services/role.service.ts
+++ src/app/services/role.service.ts
@@ -1,11 +1,11 @@
 import { Role } from "@app/shared/types/user.type";
 import { StorageService } from "@core/services/storage/storage.service";
 
-class _RoleSerivice extends StorageService {
+class _RoleService extends StorageService {
   public getRole(): string {
     return this.get("role") || this.getSession("role") || Role.GUEST;
   }
 }
 
-const RoleService = new _RoleSerivice();
+const RoleService = new _RoleService();
 export default RoleService;
