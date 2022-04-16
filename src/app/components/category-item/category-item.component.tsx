28f99e94cb1987d9be12ac5b7a4330d1be378984
Tất Trung <buitrungt@gmail.com>
2022-04-16 10:07:02 +0700

    feat: categoy list page

--- src/app/components/category-item/category-item.component.tsx
+++ src/app/components/category-item/category-item.component.tsx
@@ -1,23 +1,37 @@
+import { Link } from "react-router-dom";
 import { imageCategory } from "@app/shared/constants/common";
+import { Category } from "@app/models/category.model";
 import "./category-item.style.scss";
 
-function CategoryItem() {
+type PropTypes = {
+  item: Category;
+};
+
+function CategoryItem(props: PropTypes) {
+  const { item } = props;
+
   return (
-    <div className="category-item-wrapper">
-      <div className="category-item">
-        <img src={imageCategory} alt="" className="category-image" />
-        <div className="category-right">
-          <div className="category-text-wrapper">
-            <span className="category-text">TitleTitleT itleTitleTitle</span>
-          </div>
-          <div className="category-description-wrapper">
-            <span className="category-description">
-              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-            </span>
+    <Link
+      to={`/products?category=${item.slug}`}
+      style={{
+        textDecoration: "none",
+        color: "black",
+      }}
+    >
+      <div className="category-item-wrapper">
+        <div className="category-item">
+          <img src={imageCategory} alt="" className="category-image" />
+          <div className="category-right">
+            <div className="category-text-wrapper">
+              <span className="category-text">{item.name}</span>
+            </div>
+            <div className="category-description-wrapper">
+              <span className="category-description">{item.description}</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
--- src/app/pages/category-list/category-list.page.tsx
+++ src/app/pages/category-list/category-list.page.tsx
@@ -1,12 +1,57 @@
+import { useEffect, useState } from "react";
+import { Box, Grid } from "@material-ui/core";
+import { Pagination } from "@material-ui/lab";
 import { Helmet } from "react-helmet-async";
 import AppBar from "@app/components/app-bar";
-import { Box, Grid } from "@material-ui/core";
 import CustomBreadcrumbs from "@app/components/custom-breadcrumbs";
 import CategoryItem from "@app/components/category-item";
+import useObservable from "@core/hooks/use-observable.hook";
+import { Category } from "@app/models/category.model";
+import CategoryService, {
+  CategoryPaginationOption,
+} from "@app/services/http/category.service";
+import { DEFAULT_PAGINATION_OPTION } from "@app/shared/constants/common";
+import { ResponseResult } from "@core/services/http/http.service";
+import Footer from "@app/components/footer";
 import { useStyles } from "./make-style";
 
 function CategoryList() {
   const classes = useStyles();
+
+  const { subscribeUntilDestroy } = useObservable();
+
+  const [numberOfPage, setNumberOfPage] = useState(0);
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [pagination, setPagination] = useState(() => {
+    const options: CategoryPaginationOption = {
+      ...DEFAULT_PAGINATION_OPTION,
+      perPage: 32,
+    };
+    return options;
+  });
+
+  useEffect(() => {
+    subscribeUntilDestroy(
+      CategoryService.getList(pagination),
+      (response: ResponseResult) => {
+        const data = response.data as Category[];
+        const total = response.pagination?.total || 0;
+        const perPage = response.pagination?.perPage || 1;
+        setCategories(data);
+        setNumberOfPage(Math.ceil(total / perPage));
+      }
+    );
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [pagination]);
+
+  const onPageChange = (event: React.ChangeEvent<any>, page: number) => {
+    const options: CategoryPaginationOption = {
+      ...pagination,
+      page,
+    };
+    setPagination(options);
+  };
 
   return (
     <>
@@ -29,16 +74,28 @@ function CategoryList() {
         <Box marginTop={5}>
           <div className={classes.rootItem}>
             <Grid container spacing={4}>
-              <Grid item xs={6} sm={6}>
-                <CategoryItem />
-              </Grid>
-              <Grid item xs={6} sm={6}>
-                <CategoryItem />
-              </Grid>
+              {!!categories.length &&
+                categories.map((item, index) => (
+                  <Grid key={index} item xs={6} sm={6}>
+                    <CategoryItem item={item} />
+                  </Grid>
+                ))}
             </Grid>
           </div>
         </Box>
       </Box>
+      {!!categories.length && (
+        <Box marginTop={5}>
+          <Pagination
+            count={numberOfPage}
+            onChange={onPageChange}
+            page={pagination.page}
+            color="primary"
+            style={{ justifyContent: "center", display: "flex" }}
+          />
+        </Box>
+      )}
+      <Footer />
     </>
   );
 }
